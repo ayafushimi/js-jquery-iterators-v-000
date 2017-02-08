165c0b6555166f9435743fb33c642618f725e6d5
ayafushimi <aya.fushimi@gmail.com>
2017-02-08 17:05:14 -0500

    Done

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
 
+  var names = ["Carleton", "Avi", "Azat", "Ann"]
+  $.each(names, function(index, name){
+    console.log("hey" + name);
+  });
+
+  $('div').each(function(index, div) {
+    $(this).append("this is div number" + (index + 1));
+  });
+
+  $.map(names, function(name, index) {
+    return name + " loves JavaScript";
+  });
 
 });
+
+function listIterate() {
+  return $('li').map(function(index, item) {
+    return item.innerHTML;
+  });
+}
